5c7c37ac62647998b0ffcc4fc3b050abdc29ea93
shaikmuneerGIT <64253809+shaikmuneerGIT@users.noreply.github.com>
2020-05-10 06:02:46 +0530

    Add files via upload

---
+++ SeleniumForMuneer/SearchGoogle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumForMuneer
+{
+    [TestClass]
+    public class SearchGoogle
+    {
+        [TestMethod]
+        public void SearchForMuneer()
+        {
+            var chromeoptions = new ChromeOptions();
+            chromeoptions.AddArguments("headless");
+
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeoptions)) 
+            {
+                driver.Navigate().GoToUrl("https://google.com");
+                IWebElement query = driver.FindElement(By.Name("q"));
+                query.SendKeys("Cheese");
+                query.Submit();
+                // Conflict the code change made by user Muneer
+            }
+        }
+    }
+}
